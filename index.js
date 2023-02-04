48735d35292517b5a2eb6af06dc00e824662a7ba
amankasat <amankasat7@gmail.com>
2023-02-05 03:19:23 +0530

    fixed carousel

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-let items = document.querySelectorAll('.carousel .carousel-item')
+let items = document.querySelectorAll('.cFiction .ciFic')
 
 items.forEach((el) => {
     const minPerSlide = 4
@@ -7,6 +7,22 @@ items.forEach((el) => {
         if (!next) {
             // wrap carousel by using first child
         	next = items[0]
+      	}
+        let cloneChild = next.cloneNode(true)
+        el.appendChild(cloneChild.children[0])
+        next = next.nextElementSibling
+    }
+})
+
+let items2 = document.querySelectorAll('.cNFiction .ciNFic')
+
+items2.forEach((el) => {
+    const minPerSlide = 4
+    let next = el.nextElementSibling
+    for (var i=1; i<minPerSlide; i++) {
+        if (!next) {
+            // wrap carousel by using first child
+        	next = items2[0]
       	}
         let cloneChild = next.cloneNode(true)
         el.appendChild(cloneChild.children[0])
